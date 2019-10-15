206f2f811adbe3315a069893f93f53aadabd1aa5
Dusty Miller <dusty.miller109@gmail.com>
2019-10-15 10:19:40 -0400

    Update sysmon.ps1
    Made the checked string more easily changeable for update configurations.

--- sysmon.ps1
+++ sysmon.ps1
@@ -1,8 +1,9 @@
 $sysmon = sysmon -s | select-string schemaversion=
 $sysmonsource = '<set your source location here>'
 $sysmonconfig = '<set your xml location here>'
+$sysmonstring = '<manifest schemaversion="4.21" binaryversion="9.10">'
 
-IF ($sysmon.ToString() -eq '<manifest schemaversion="4.21" binaryversion="9.10">') {
+IF ($sysmon.ToString() -eq $sysmonstring) {
     & $sysmonsource -c $sysmonconfig
 } ELSE {
     # Uninstall sysmon and re-install new version
