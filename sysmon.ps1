f0b7f64ccdc9ac134a9285de0401e4b3bba5823e
Dusty Miller <dusty.miller109@gmail.com>
2019-11-11 10:37:04 -0500

    Fix and update powershell script.

--- sysmon.ps1
+++ sysmon.ps1
@@ -1,12 +1,22 @@
 $sysmon = sysmon -s | select-string schemaversion=
 $sysmonsource = '<set your source location here>'
 $sysmonconfig = '<set your xml location here>'
-$sysmonstring = '<manifest schemaversion="4.21" binaryversion="9.10">'
+$sysmonstring = '<manifest schemaversion="4.22" binaryversion="9.20">'
 
-IF ($sysmon.ToString() -eq $sysmonstring) {
-    & $sysmonsource -c $sysmonconfig
+IF (Test-Path $filePath) {
+    IF ($sysmon.ToString() -eq '<manifest schemaversion="4.22" binaryversion="9.20">') {
+        & $sysmonsource -c $sysmonconfig
+    } ELSE {
+        # Uninstall sysmon and re-install new version
+        sysmon -u
+        & $sysmonsource -i $sysmonconfig -acceptEula
+    }
 } ELSE {
-    # Uninstall sysmon and re-install new version
-    sysmon -u
     & $sysmonsource -i $sysmonconfig -acceptEula
+}
+
+$maxlength = Get-WinEvent -ListLog Microsoft-Windows-Sysmon/Operational
+IF ($maxlength -ne 524288000) {
+    $maxlength.MaximumSizeInBytes = 524288000
+    $maxlength.SaveChanges()
 }
